3f6cffa230057d868dece203a624e72efec4b4cb
Amin Al Hazwani <aminalhazwani@users.noreply.github.com>
2018-12-10 00:19:56 +0100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Remove outdated versions from the cellar.
+brew cleanup
