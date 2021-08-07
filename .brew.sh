fe81e86e2c80a8af6e7c8af728b33bbaabb0587b
Amin Al Hazwani <aminalhazwani@users.noreply.github.com>
2021-08-07 17:34:38 +0200

    Update .brew.sh

--- .brew.sh
+++ .brew.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools using Homebrew
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we’re using the latest Homebrew
 brew update
 
-# Upgrade any already-installed formulae.
+# Upgrade any already-installed formulae
 brew upgrade
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GnuPG to enable PGP-signing commits.
+# Install GnuPG to enable PGP-signing commits
 brew install gnupg
 brew install pinentry-mac
 
-# Install other useful binaries.
+# Install other useful binaries
 brew install git
 
 # Disable analytics
 brew analytics off
 
-# Remove outdated versions from the cellar.
+# Remove outdated versions from the cellar
 brew cleanup
